1e4505878f00f615023db60f2d4cf887e7080c82
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-25 18:47:45 -0300

    subindo broad cast de airplane mode

--- android/app/src/main/kotlin/com/example/flutter_challenge/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_challenge/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.flutter_challenge
 
-import android.app.Activity
-import android.content.ComponentName
-import android.content.pm.PackageManager
+import android.content.Intent
+import android.content.IntentFilter
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity: FlutterActivity() {
 
-    private val networkEventChannel = "platform_channel_events/connectivity"
+    private val connectionEventChannel = "platform_channel_events/connectivity"
+    private val airPlaneEventChannel = "platform_channel_events/airplaneMode"
+    val streamHandelr = AirPlaneModeStreamHandler()
+
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        EventChannel(flutterEngine.dartExecutor.binaryMessenger, networkEventChannel)
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, connectionEventChannel)
                 .setStreamHandler(NetworkStreamHandler(this))
 
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, airPlaneEventChannel)
+                .setStreamHandler(streamHandelr)
+
+
+        val receiver = AirPlaneModeReceiver(streamHandelr)
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        this.registerReceiver(receiver, intentFilter)
     }
 }
--- lib/src/presentation/modules/home/home_page.dart
+++ lib/src/presentation/modules/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_challenge/src/core/base/widget.dart';
 import 'package:flutter_challenge/src/core/channel/connectivity_channel.dart';
 import 'package:flutter_challenge/src/core/di/di_handler.dart';
@@ -61,17 +64,19 @@ class MottuAppBar extends StatelessWidget with BaseWidgetStateless  implements P
 
   get connectionHandler => get<ConnectivityEventChannel>();
 
+  var airplaneChannel = AirPlaneChannel()..listenTo();
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Column(
         children: [
-          StreamBuilder<Connection?>(
-              stream: connectionHandler.controller?.stream,
+          StreamBuilder<AirplaneMode?>(//Connection?>(
+              stream: airplaneChannel.controller?.stream,//connectionHandler.controller?.stream,
               builder: (context, snapshot) {
                 return Visibility(
-                  visible: snapshot.data != null ||  snapshot.data != Connection.notListening,
+                  visible: snapshot.data != null,//||  snapshot.data != Connection.notListening,
                   child: Padding(
                     padding: EdgeInsets.only(
                       top: MediaQuery.of(context).viewPadding.top
@@ -79,7 +84,7 @@ class MottuAppBar extends StatelessWidget with BaseWidgetStateless  implements P
                     child: Container(
                       width: double.infinity,
                       height: 40,
-                      color: getConnectionColor(snapshot.data),
+                      color: Colors.orange,//getConnectionColor(snapshot.data),
                       child: Center(
                         child: Text(
                           snapshot.data?.name.toString() ?? "",
@@ -117,4 +122,42 @@ class MottuAppBar extends StatelessWidget with BaseWidgetStateless  implements P
 
 
   }
+}
+
+class AirPlaneChannel {
+  StreamController<AirplaneMode?>? controller;
+
+  reset(){
+    controller?.sink.add(null);
+    controller?.sink.close();
+  }
+
+
+  Stream stream = const EventChannel("platform_channel_events/airplaneMode")
+      .receiveBroadcastStream()
+      .distinct();
+
+  listenTo(){
+    controller = StreamController();
+
+    stream.listen((event) {
+      if(event != null){
+        if(!(controller?.isClosed ?? true)) {
+          if(event is bool){
+            controller?.sink.add(event ? AirplaneMode.ON : AirplaneMode.OFF);
+          }
+          else {
+            controller?.sink.add(AirplaneMode.DESCONHECIDO);
+          }
+        }
+      }
+    });
+  }
+
+}
+
+enum AirplaneMode{
+  ON,
+  OFF,
+  DESCONHECIDO
 }

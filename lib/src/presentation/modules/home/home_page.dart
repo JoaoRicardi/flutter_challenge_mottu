0cfbc8dbd350bde72333e4c695b176e35569d3c5
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-28 19:05:03 -0300

    comencando os testes unitarios

--- lib/src/presentation/modules/home/home_page.dart
+++ lib/src/presentation/modules/home/home_page.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_challenge/src/core/base/widget.dart';
-import 'package:flutter_challenge/src/core/channel/airplane/airplane_stream_channel.dart';
+import 'package:flutter_challenge/src/core/di/di_handler.dart';
 import 'package:flutter_challenge/src/core/di/di_handler_imp.dart';
 import 'package:flutter_challenge/src/presentation/modules/home/controller/home_controller.dart';
 import 'package:flutter_challenge/src/presentation/widgtes/configuration_item.dart';
@@ -22,9 +21,18 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with BaseWidgetStateful{
+class _HomePageState extends State<HomePage> with BaseWidget<HomeController>{
 
-  get controller => get<HomeController>();
+
+  @override
+  late HomeController controller;
+
+  @override
+  void initState() {
+    controller = inject.get();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +41,6 @@ class _HomePageState extends State<HomePage> with BaseWidgetStateful{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
             Observer(
                 builder: (context) {
                   return ConfigurationItem(
@@ -58,6 +65,20 @@ class _HomePageState extends State<HomePage> with BaseWidgetStateful{
       ),
     );
   }
+}
+
+abstract class BasePage<T extends Object> {
+   late T controller;
+}
+
+class BaseWidget<T extends Object> implements BasePage<T> {
+  IDIHandler inject = DIHandlerImp();
+
+  @override
+  late T controller;
+
+
+
 
 }
 
